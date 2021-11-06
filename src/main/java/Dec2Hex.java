49c7a726aa9c2f06d4322f9c14e972e38bb71fc4
Phunkyize <andreys.kostins@gmail.com>
2021-11-06 14:16:43 +0000

    code improvements, more error handling

--- src/main/java/Dec2Hex.java
+++ src/main/java/Dec2Hex.java
@@ -5,33 +5,33 @@ class Dec2Hex {
 
     public static void main(String[] args) {
         if (args.length != 0) {
-            boolean success = false;
+            Arg1 = 0;
             try {
-                int i = Integer.parseInt(args[0]);
-                success = true;
-            } catch (NumberFormatException e){
-                System.out.println("Provided input is not a number");
+                Arg1 = Integer.parseInt(args[0]);
+            } catch (NumberFormatException e) {
+                System.out.print("Provided input is not a number");
                 return;
             }
-            if (success) {
-                System.out.println(args.length);
-                Arg1 = Integer.parseInt(args[0]);
-                char ch[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
-                int rem, num;
-                num = Arg1;
-                String hexadecimal = "";
-                System.out.println("Converting the Decimal Value " + num + " to Hex...");
-
-                while (num != 0) {
-                    rem = num % 16;
-                    hexadecimal = ch[rem] + hexadecimal;
-                    num = num / 16;
-                }
-
-                System.out.println("Hexadecimal representation is: " + hexadecimal);
-            }
+            ConvertDecimalToHex(Arg1);
         } else {
             throw new ArrayIndexOutOfBoundsException("No argument input provided");
         }
+    }
+
+    public static void ConvertDecimalToHex(int i) {
+        char[] ch = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+        int rem, num;
+        num = i;
+        StringBuilder hexadecimal = new StringBuilder();
+        System.out.println("Converting the Decimal Value " + num + " to Hex...");
+
+        while (num != 0) {
+            rem = num % 16;
+            hexadecimal.insert(0, ch[rem]);
+            num = num / 16;
+        }
+
+        System.out.println("Hexadecimal representation is: " + hexadecimal);
+
     }
 }
--- src/test/java/Dec2HexTest.java
+++ src/test/java/Dec2HexTest.java
@@ -1,8 +1,13 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 public class Dec2HexTest {
 
+
+    //test for zero argument error throw as a requirement
     @Test
     void ZeroArgumentErrorThrow() {
         Throwable e = null;
@@ -12,6 +17,32 @@ public class Dec2HexTest {
             e = err;
         }
 
+        //if caught error equals of type ArrayIndexOutOfBoundsException, test passed
         Assertions.assertTrue(e instanceof ArrayIndexOutOfBoundsException);
+    }
+
+    //test for non-integer input error caught but not thrown with correct message displayed
+    @Test
+    void NonIntegerInputHandling() {
+        Throwable e = null;
+
+        ByteArrayOutputStream newConsole = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(newConsole));
+
+        try {
+            Dec2Hex.main(new String[]{"number"});
+        } catch (Throwable err) {
+            e = err;
+        }
+
+        //assert null if no error thrown - PASS if error handling working as intended
+        Assertions.assertNull(e);
+
+        //assert equals for console output - PASS if console output equals as follows
+        Assertions.assertEquals("Provided input is not a number", newConsole.toString());
+
+        //assert false for console output - PASS if console output doesn't contain the following message
+        //indicates that conversion code will not execute for a non-integer number
+        Assertions.assertFalse(newConsole.toString().contains("Converting the Decimal Value"));
     }
 }
