099b6bdbc6c97a1d99383229d41dcbee3fb2709f
Phunkyize <andreys.kostins@gmail.com>
2021-11-06 14:29:01 +0000

    rever to test pass

--- src/test/java/Dec2HexTest.java
+++ src/test/java/Dec2HexTest.java
@@ -18,7 +18,7 @@ public class Dec2HexTest {
         }
 
         //if caught error equals of type ArrayIndexOutOfBoundsException, test passed
-        Assertions.assertFalse(e instanceof ArrayIndexOutOfBoundsException);
+        Assertions.assertTrue(e instanceof ArrayIndexOutOfBoundsException);
     }
 
     //test for non-integer input error caught but not thrown with correct message displayed
